228d0366a8178608dc91155c97d0c79816eb8559
tobilg <tobias.mueller@mymood.de>
2015-11-05 13:06:03 +0100

    Mesos and Marathon version update

--- pull_images.sh
+++ pull_images.sh
@@ -4,13 +4,13 @@ echo "Pulling Zookeeper"
 docker pull tobilg/zookeeper:latest
 
 echo "Pulling Mesos Master"
-docker pull tobilg/mesos-master:latest
+docker pull tobilg/mesos-master:0.25.0
 
 echo "Pulling Mesos Slave"
-docker pull tobilg/mesos-slave:latest
+docker pull tobilg/mesos-slave:0.25.0
 
 echo "Pulling Marathon"
-docker pull mesosphere/marathon:latest
+docker pull mesosphere/marathon:v0.13.0-RC1
 
 echo "Pulling Chronos"
 docker pull tobilg/chronos:latest
